ae24fb532a231a3b5fc23bc985e3d49811e0182a
joecode22 <joe.code.mckinney@gmail.com>
2023-05-29 11:47:20 -0500

    added route to post-routes.js to get a single post by id

--- controllers/post-routes.js
+++ controllers/post-routes.js
@@ -34,11 +34,40 @@ router.get('/', async (req, res) => {
     }
 });
 
-//GET request to fetch a single post
+// GET request to fetch a single post
 router.get('/:id', async (req, res) => {
     try {
         const postData = await Post.findOne({
-            where: { id: req.params.id },
+            where: {
+                id: req.params.id
+            },
             attributes: ['id', 'post_url', 'title', 'created_at',
-                [sequelize.literal('SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id')], 
+                [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+            ],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+
+        res.json(postData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
     }
+});
